7114e3d116f6b5a28ed6c63991759b8c4bb47ec1
irfanfauzi <irfanwintech@yahoo.com>
2022-09-24 23:29:12 +0700

    init

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full relative max-w-lg border border-black'>
+      
+    </div>
+  )
+}
+
+export default Header
