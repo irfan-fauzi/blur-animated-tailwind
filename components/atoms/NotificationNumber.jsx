12b9b115e6ba84fbddbf87420c6ce1ec22722e4f
irfanfauzi <irfanwintech@yahoo.com>
2022-12-01 23:02:29 +0700

    init

--- components/atoms/NotificationNumber.jsx
+++ components/atoms/NotificationNumber.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NotificationNumber = () => {
   return (
     <div className="absolute bottom-5 left-5 bg-red-600 w-[2rem] h-[2rem] p-[3px] rounded-full">
-      <p className="text-white text-center">5</p>
+      <p className="text-white text-center">4</p>
     </div>
   );
 };
